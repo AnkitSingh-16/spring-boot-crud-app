ce15c3f106d2ba577edfd318844e22978d947dd5
Ankit Singh <2808aksingh@gmail.com>
2024-09-11 13:54:58 +0530

    [IMP] spring crud app: added H2 database, spring data jpa, and connecting app to database

--- src/main/java/com/javaapp/demo/repository/ProductRepo.java
+++ src/main/java/com/javaapp/demo/repository/ProductRepo.java
@@ -1,8 +1,11 @@
 package com.javaapp.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.javaapp.demo.model.Product;
+
 @Repository
-public interface ProductRepo {
+public interface ProductRepo extends JpaRepository<Product, Integer> {
 
 }
