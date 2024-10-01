f45689e7e621458ceafe27b91f45a3a520959fa1
Ankit Singh <2808aksingh@gmail.com>
2024-10-01 11:56:32 +0530

    Genereated csrf token

---
+++ src/main/java/com/javaapp/demo/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.javaapp.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity //(this annotaion is used to tell that use this method for security)
+public class SecurityConfig {
+
+}
--- src/main/java/com/javaapp/demo/controller/HomeController.java
+++ src/main/java/com/javaapp/demo/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.javaapp.demo.controller;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HomeController {
@@ -10,4 +14,13 @@ public class HomeController {
         System.out.println("hi there...");
         return "Welcome to ak page";
     }
+
+    // this method is used to get the csrf token
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+
+
+    }
+
 }
